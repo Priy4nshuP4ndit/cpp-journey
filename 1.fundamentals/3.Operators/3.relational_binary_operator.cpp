d2d14fe9b24f6af90c7168906d8542d4fa241a4c
= <=>
2026-07-31 14:26:09 +0530

    added 3.relational_binary_operator.cpp in 3.Operators folder

---
+++ 1.fundamentals/3.Operators/3.relational_binary_operator.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+    // == equals to
+    // != not equals to
+    // < less than
+    // <= less than or equals to
+    // > greater than
+    // >= greater than equals to
+
+    int x = 10 , y = 20 , z= 10;
+    cout<<"is 10 = 10: " << (x==z) << endl;
+    cout<<"is 10 != 10: " << (x!=z) << endl;
+    cout<<"is 10 < 20: " << (x<y) << endl;
+    cout<<"is 10 <= 20: " << (x<=y) << endl;
+     cout<<"is 10 >= 20: " << (x>=y) << endl;
+
+    return 0;
+}
+    
+    
+

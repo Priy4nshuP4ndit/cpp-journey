0ce7da4c8dfbe59153c5a432799b0a9de2752f2d
Priy4nshuP4ndit <priyanshupandit0101@gmail.com>
2026-08-02 22:21:28 +0530

    assignment operator

---
+++ 1.fundamentals/3.Operators/4.assignment_operator.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int i = 10;
+    cout << "currently i is: " << i << endl;
+    cout << "i+=2: " << (i+=2) <<endl;
+   cout << "currently i is: " << i << endl;
+   cout << "i-=2: " << (i-=2) <<endl;
+   cout << "currently i is: " << i << endl;
+   cout << "i*=2: " << (i*=2) <<endl;
+   cout << "currently i is: " << i << endl;
+   cout << "i/=2: " << (i/=2) <<endl;
+     cout << "currently i is: " << i << endl;
+cout << "i%=2: " << (i%=2) <<endl;
+     cout << "currently i is: " << i << endl;
+    return 0;
+}
